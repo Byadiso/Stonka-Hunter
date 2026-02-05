55cf01ab0befb39b08e0091fa13f1f5e911b12b8
unknown <byadiso@gmail.com>
2026-02-05 01:23:06 +0100

    fix numpy in deployment to streamlit adding runtime file to force streamlit to use py 3.10

--- script.py
+++ script.py
@@ -6,12 +6,6 @@ import requests
 from ultralytics import YOLO
 from streamlit_geolocation import streamlit_geolocation
 from datetime import datetime
-import sys
-
-st.write("✅ Python version:", sys.version)
-st.write("✅ NumPy version:", np.__version__)
-st.write("✅ OpenCV version:", cv2.__version__)
-
 
 st.set_page_config(
     page_title="Pest Hunter PRO",
